a166fe79efa531411e42a898fbe410c13b81cb9d
Lia <aliyahrenomeron7@gmail.com>
2026-03-26 21:48:50 -0400

    adjusted the js to have the text write on

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
-// Wait until everything is loaded
+// Wait for the page to load 
 window.addEventListener("load", function () {
-  let startBtn = document.getElementById("start-btn");
+  const welcomeEl = document.getElementById("welcome-el");
+  const globeEl = document.getElementById("globe-gif");
+  const startEl = document.getElementById("start-btn");
+  const startBtn = document.getElementById("start-btn");
 
+  const welcomeText = "WELCOME TO THE WORLD WIDE WEB";
+  const speed = 150;
+  let i = 0;
+
+  welcomeEl.textContent = "";
+
+  // Have the text write on
+  function typeWriter() {
+    if (i < welcomeText.length) {
+      welcomeEl.innerHTML += welcomeText.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+  // Function to make the button clickable
   startBtn.addEventListener("click", function () {
     // console.log("Button is clicked")
     window.location.href = "#";
   });
 
-  setTimeout(function () {
-    let webEl = document.getElementById("welcome-el");
-    let globeEl = document.getElementById("globe-gif");
-    let startEl = document.getElementById("start-btn");
-
-    webEl.style.opacity = 1;
+  // Wait 2 seconds after the page loads to fade in the elements
+  this.setTimeout(function () {
+    welcomeEl.style.opacity = 1;
     globeEl.style.opacity = 1;
     startEl.style.opacity = 1;
   }, 2000);
+
+  // Wait 4 seconds after the page loads to fade in the welcome text
+  this.setTimeout(function () {
+    typeWriter();
+  }, 4000);
+
 });

ce568871f86716952645c4177a0bc4b7d7acc8f1
Lia <aliyahrenomeron7@gmail.com>
2026-04-19 21:01:41 -0400

    added the ad window

--- js/Ads.js
+++ js/Ads.js
@@ -1,6 +1,8 @@
 // Variables
 const startBtn = document.getElementById("start-btn");
+const adWindow = document.getElementById("ad-window");
 
 startBtn.addEventListener("click", function() {
     startBtn.style.opacity = 0;
+    adWindow.classList.toggle("show");
 });

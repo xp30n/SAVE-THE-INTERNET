cd5ae42fbf30348923b384766924503f4ac58401
Lia <aliyahrenomeron7@gmail.com>
2026-04-21 11:16:10 -0400

    added the new fake taskbar

--- js/homescreen.js
+++ js/homescreen.js
@@ -1,11 +1,5 @@
 window.addEventListener("keydown", (event) => {
-    if (event.key === "Z" || event.key === "z") {
-        window.location.href = "../index.html"
-    }
-})
-
-const taskbarImg = document.getElementById("taskbar");
-
-setTimeout( function() {
-    taskbarImg.style.opacity = 1;
-}, 1000);
+  if (event.key === "Z" || event.key === "z") {
+    window.location.href = "../index.html";
+  }
+});

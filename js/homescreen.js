d09a5e63b0816297914fd46d8738d65f0a835b81
Lia <aliyahrenomeron7@gmail.com>
2026-04-21 12:14:59 -0400

    added a warning msg

--- js/homescreen.js
+++ js/homescreen.js
@@ -1,7 +1,24 @@
 const loginBtn = document.getElementById("footer-right");
 
 loginBtn.addEventListener("click", function() {
-    console.log("You can't return to login!");
+    // console.log("You can't return to login!");
+
+    const newDiv = document.createElement("div");
+
+    newDiv.textContent = "You can't return to login!"
+    newDiv.className = "login-return"
+
+    document.body.appendChild(newDiv);
+
+    // After two seconds, the warning will fade out
+    setTimeout(() => {
+        newDiv.classList.add("fade-out");
+    }, 2000)
+
+    // After 2.5 seconds, the div will be gone
+    setTimeout(() => {
+        newDiv.remove();
+    }, 2500)
 })
 
 window.addEventListener("keydown", (event) => {

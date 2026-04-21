1c40ef42811f3f90b2b2b214579a83aa1fa79ef0
Lia <aliyahrenomeron7@gmail.com>
2026-04-21 11:39:08 -0400

    Added a return function

--- js/homescreen.js
+++ js/homescreen.js
@@ -1,5 +1,12 @@
+const loginBtn = document.getElementById("footer-right");
+
+loginBtn.addEventListener("click", function() {
+    console.log("You can't return to login!");
+})
+
 window.addEventListener("keydown", (event) => {
   if (event.key === "Z" || event.key === "z") {
     window.location.href = "../index.html";
   }
 });
+

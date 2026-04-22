b3f56346953ca0a037cf181ce06c7a6d5537de5a
Lia <aliyahrenomeron7@gmail.com>
2026-04-22 19:35:43 -0400

    fixed the homepage button

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function createWindow() {
     })
 
     win.loadFile('index.html');
-    // win.webContents.openDevTools();
+    win.webContents.openDevTools();
     // win.setAlwaysOnTop(true);
 }
 
